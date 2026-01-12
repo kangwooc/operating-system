d3056b994ca60a22c9f92a354b973533190817b5
Kangwoo choi <kangwooc@gmail.com>
2026-01-12 15:57:48 +0900

    process

--- kernel.c
+++ kernel.c
@@ -6,6 +6,132 @@ extern char __bss[], __bss_end[], __stack_top[];
 // 페이지 크기 정의
 extern char __free_ram[], __free_ram_end[];
 
+#define PROCS_MAX 8       // 최대 프로세스 개수
+
+#define PROC_UNUSED   0   // 사용되지 않는 프로세스 구조체
+#define PROC_RUNNABLE 1   // 실행 가능한(runnable) 프로세스
+
+struct process {
+    int pid;             // 프로세스 ID
+    int state;           // 프로세스 상태: PROC_UNUSED 또는 PROC_RUNNABLE
+    vaddr_t sp;          // 스택 포인터
+    uint8_t stack[8192]; // 커널 스택
+};
+
+
+__attribute__((naked)) void switch_context(uint32_t *prev_sp,
+                                           uint32_t *next_sp) {
+    __asm__ __volatile__(
+        // 현재 프로세스의 스택에 callee-saved 레지스터를 저장
+        "addi sp, sp, -13 * 4\n" // 13개(4바이트씩) 레지스터 공간 확보
+        "sw ra,  0  * 4(sp)\n"   // callee-saved 레지스터만 저장
+        "sw s0,  1  * 4(sp)\n"
+        "sw s1,  2  * 4(sp)\n"
+        "sw s2,  3  * 4(sp)\n"
+        "sw s3,  4  * 4(sp)\n"
+        "sw s4,  5  * 4(sp)\n"
+        "sw s5,  6  * 4(sp)\n"
+        "sw s6,  7  * 4(sp)\n"
+        "sw s7,  8  * 4(sp)\n"
+        "sw s8,  9  * 4(sp)\n"
+        "sw s9,  10 * 4(sp)\n"
+        "sw s10, 11 * 4(sp)\n"
+        "sw s11, 12 * 4(sp)\n"
+
+        // 스택 포인터 교체
+        "sw sp, (a0)\n"         // *prev_sp = sp
+        "lw sp, (a1)\n"         // sp를 다음 프로세스의 값으로 변경
+
+        // 다음 프로세스 스택에서 callee-saved 레지스터 복원
+        "lw ra,  0  * 4(sp)\n"  
+        "lw s0,  1  * 4(sp)\n"
+        "lw s1,  2  * 4(sp)\n"
+        "lw s2,  3  * 4(sp)\n"
+        "lw s3,  4  * 4(sp)\n"
+        "lw s4,  5  * 4(sp)\n"
+        "lw s5,  6  * 4(sp)\n"
+        "lw s6,  7  * 4(sp)\n"
+        "lw s7,  8  * 4(sp)\n"
+        "lw s8,  9  * 4(sp)\n"
+        "lw s9,  10 * 4(sp)\n"
+        "lw s10, 11 * 4(sp)\n"
+        "lw s11, 12 * 4(sp)\n"
+        "addi sp, sp, 13 * 4\n" 
+        "ret\n"
+    );
+}
+
+struct process procs[PROCS_MAX]; // 모든 프로세스 제어 구조체 배열
+
+struct process *create_process(uint32_t pc) {
+    // 미사용(UNUSED) 상태의 프로세스 구조체 찾기
+    struct process *proc = NULL;
+    int i;
+    for (i = 0; i < PROCS_MAX; i++) {
+        if (procs[i].state == PROC_UNUSED) {
+            proc = &procs[i];
+            break;
+        }
+    }
+
+    if (!proc)
+        PANIC("no free process slots");
+
+    // 커널 스택에 callee-saved 레지스터 공간을 미리 준비
+    // 첫 컨텍스트 스위치 시, switch_context에서 이 값들을 복원함
+    uint32_t *sp = (uint32_t *) &proc->stack[sizeof(proc->stack)];
+    *--sp = 0;                      // s11
+    *--sp = 0;                      // s10
+    *--sp = 0;                      // s9
+    *--sp = 0;                      // s8
+    *--sp = 0;                      // s7
+    *--sp = 0;                      // s6
+    *--sp = 0;                      // s5
+    *--sp = 0;                      // s4
+    *--sp = 0;                      // s3
+    *--sp = 0;                      // s2
+    *--sp = 0;                      // s1
+    *--sp = 0;                      // s0
+    *--sp = (uint32_t) pc;          // ra (처음 실행 시 점프할 주소)
+
+    // 구조체 필드 초기화
+    proc->pid = i + 1;
+    proc->state = PROC_RUNNABLE;
+    proc->sp = (uint32_t) sp;
+    return proc;
+}
+
+struct process *current_proc; // 현재 실행 중인 프로세스
+struct process *idle_proc;    // Idle 프로세스
+
+void yield(void) {
+    // 실행 가능한 프로세스를 탐색
+    struct process *next = idle_proc;
+    for (int i = 0; i < PROCS_MAX; i++) {
+        struct process *proc = &procs[(current_proc->pid + i) % PROCS_MAX];
+        if (proc->state == PROC_RUNNABLE && proc->pid > 0) {
+            next = proc;
+            break;
+        }
+    }
+
+    // 현재 프로세스 말고는 실행 가능한 프로세스가 없으면, 그냥 리턴
+    if (next == current_proc)
+        return;
+
+    // 스위칭 시 sscratch에 커널 스택 초기값을 설정 합니다. 아래와 같이 스케줄러(yield)에서 프로세스 전환 직전에 sscratch 레지스터를 적절히 설정
+    __asm__ __volatile__(
+        "csrw sscratch, %[sscratch]\n"
+        :
+        : [sscratch] "r" ((uint32_t) &next->stack[sizeof(next->stack)])
+    );
+
+    // 컨텍스트 스위칭
+    struct process *prev = current_proc;
+    current_proc = next;
+    switch_context(&prev->sp, &next->sp);
+}
+
 paddr_t alloc_pages(uint32_t n) {
     // next_paddr은 static 변수로 선언되었기 때문에, 로컬 변수와 달리 함수 호출이 끝나도 값을 계속 유지합니다(글로벌 변수처럼 동작).
     // next_paddr는 “새로 할당할 메모리 영역의 시작 주소"
@@ -47,6 +173,34 @@ void putchar(char ch) {
     sbi_call(ch, 0, 0, 0, 0, 0, 0, 1 /* Console Putchar */);
 }
 
+void delay(void) {
+    for (int i = 0; i < 30000000; i++)
+        __asm__ __volatile__("nop"); // do nothing
+}
+
+struct process *proc_a;
+struct process *proc_b;
+
+void proc_a_entry(void) {
+    printf("starting process A\n");
+    while (1) {
+        putchar('A');
+        // switch_context(&proc_a->sp, &proc_b->sp);
+        // delay();
+        yield();
+    }
+}
+
+void proc_b_entry(void) {
+    printf("starting process B\n");
+    while (1) {
+        putchar('B');
+        // switch_context(&proc_b->sp, &proc_a->sp);
+        // delay();
+        yield();
+    }
+}
+
 // 커널 진입점(entry point) 함수
 // 이 함수는 어셈블리어로 작성되어 있으며, 트랩이 발생했을 때 호출됩니다.
 // sscratch 레지스터를 임시 저장소로 이용해 예외 발생 시점의 스택 포인터를 저장해두고, 나중에 복구합니다.
@@ -57,7 +211,10 @@ __attribute__((naked))
 __attribute__((aligned(4)))
 void kernel_entry(void) {
     __asm__ __volatile__(
-        "csrw sscratch, sp\n"
+        // Retrieve the kernel stack of the running process from sscratch.
+        // csrrw sp, sscratch, sp는 사실상 스왑(swap) 연산으로 보면 됩니다:
+        // 예외가 발생했을 때의 스택 포인터를 신뢰할 수 없기 때문에, sscratch에 미리 저장해둔 커널 스택 포인터로 교체합니다.
+        "csrrw sp, sscratch, sp\n"
         "addi sp, sp, -4 * 31\n"
         "sw ra,  4 * 0(sp)\n"
         "sw gp,  4 * 1(sp)\n"
@@ -90,8 +247,13 @@ void kernel_entry(void) {
         "sw s10, 4 * 28(sp)\n"
         "sw s11, 4 * 29(sp)\n"
 
+        // Retrieve and save the sp at the time of exception.
         "csrr a0, sscratch\n"
         "sw a0, 4 * 30(sp)\n"
+
+        // Reset the kernel stack.
+        "addi a0, sp, 4 * 31\n"
+        "csrw sscratch, a0\n"
 
         "mv a0, sp\n"
         "call handle_trap\n"
@@ -147,11 +309,21 @@ void kernel_main(void) {
     printf("alloc_pages test: paddr0=%x\n", paddr0);
     printf("alloc_pages test: paddr1=%x\n", paddr1);
 
-    WRITE_CSR(stvec, (uint32_t) kernel_entry); // new
+    WRITE_CSR(stvec, (uint32_t) kernel_entry);
+
+    idle_proc = create_process((uint32_t) NULL);
+    idle_proc->pid = 0; // idle
+    current_proc = idle_proc;
+
+    proc_a = create_process((uint32_t) proc_a_entry);
+    proc_b = create_process((uint32_t) proc_b_entry);
+
+    yield();
+    PANIC("switched to idle process");
 
     // stvec를 설정한 뒤, unimp 명령어(illegal instruction 로 간주됨)를 실행해 일부러 예외를 일으키는 코드입니다.
     // unimp 는 “의사(pseudo) 명령어”.
-    __asm__ __volatile__("unimp"); // new
+    // __asm__ __volatile__("unimp"); // new
 }
 
 // __attribute__는 함수나 변수에 특별한 속성을 부여하는 GCC 컴파일러의 확장 기능입니다.
